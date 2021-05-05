5e0fdcfa54e943fa7468a7750cb1f6130b7ebf5a
Ibekason Alexander <alibekason@gmail.com>
2021-05-05 23:36:22 +0100

    Add svg packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttermoneymanagementui/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: null,
+      home: HomeScreen(),
     );
   }
 }
